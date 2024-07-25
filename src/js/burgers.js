5af10c1ebc0e0f5fb908986a8452c9bb94726f01
Татьяна <ltasya72@gmail.com>
2024-07-25 18:37:21 +0300

    работа над модальным окном доставки

--- src/js/burgers.js
+++ src/js/burgers.js
@@ -3,14 +3,26 @@ if (".catalog-menu__btn") {
     $(".btn-main").toggleClass("btn-main-open");
     $(".catalog-menu__ground").toggleClass("catalog-menu__ground--open");
   });
-  //  Закрытие окна меню каталога при клике на любую точку экрана кроме меню каталога
-  document.addEventListener("click", (e) => {
-    if (!e.target.closest(".catalog-menu")) {
-      $(".btn-main").toggleClass("btn-main-open");
-      $(".catalog-menu__ground").toggleClass("catalog-menu__ground--open");
-    }
-  });
 }
+//  Закрытие окна меню каталога при клике на любую точку экрана кроме меню каталога
+
+// проверяем клик на окне
+window.addEventListener("click", function (event) {
+  // проверяем что клик был именно по кнопке "В корзину"
+  if (event.target.closest(".catalog-menu")) {
+    if ("btn-main-open") {
+      $(".btn-main").remove("btn-main-open");
+      $(".catalog-menu__ground").remove("catalog-menu__ground--open");
+    }
+  }
+});
+
+// document.addEventListener("click", (e) => {
+//   if (!e.target.closest(".catalog-menu")) {
+//     $(".btn-main").toggleClass("btn-main-open");
+//     $(".catalog-menu__ground").toggleClass("catalog-menu__ground--open");
+//   }
+// });
 
 document.addEventListener("DOMContentLoaded", () => {
   // 1 по клику на боковое меню открывать dropdown
@@ -19,6 +31,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const catalogBtns = document.querySelectorAll(".catalog-menu__btn-secondary");
   const drops = document.querySelectorAll(".dropdown");
+  const ground = document.querySelectorAll(".catalog-menu__ground");
+  const mainBtn = document.querySelectorAll(".btn-main");
+  const activeBtn = document.querySelectorAll("[data-menu-active]");
+
   catalogBtns.forEach((el) => {
     el.addEventListener("click", (e) => {
       let currentBtn = e.currentTarget;
@@ -50,6 +66,15 @@ document.addEventListener("DOMContentLoaded", () => {
       drops.forEach((el) => {
         el.classList.remove("dropdown--active");
       });
+      ground.forEach((el) => {
+        el.classList.remove("catalog-menu__ground--open");
+      });
+      mainBtn.forEach((el) => {
+        el.classList.remove("btn-main-open");
+      });
+      // console.log(activeBtn);
+      $(activeBtn).toggleClass("catalog-menu__btn-secondary--actve");
+      // activeBtn.classList.toggle("catalog-menu__btn-secondary--actve");
     }
   });
 });
---
+++ src/js/my-mixitup.js
@@ -0,0 +1,7 @@
+if (document.querySelector(".tabs-content")) {
+  var mixer = mixitup(".tabs-content", {
+    load: {
+      filter: ".delivery",
+    },
+  });
+}
