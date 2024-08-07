8e98becc1eb5f5f128fe3068d0e5b84960a42159
Татьяна <ltasya72@gmail.com>
2024-08-07 10:48:24 +0300

    Работа над aside gatalog

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ function htmlInclude() {
     "./src/html/index.html",
     "./src/html/page-product.html",
     "./src/html/page-catalog.html",
-    "./src/html/select.html",
+    // "./src/html/select.html",
   ])
     .pipe(
       fileinclude({
@@ -162,7 +162,7 @@ function scripts() {
       // "node_modules/simplelightbox/dist/simple-lightbox.min.js",
       // "node_modules/siema/dist/siema.min.js",
       // "node_modules/aos/dist/aos.js",
-      // "libs/formstyler/jquery.formstyler.min.js",
+      "libs/formstyler/jquery.formstyler.min.js",
       // "js/ion.rangeSlider.min.js",
       // "js/jquery.rateyo.js",
       // "./src/js/burger.js",
