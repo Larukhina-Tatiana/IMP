0046ef42821e02c71da156f7f5f71f01dd270259
Татьяна <ltasya72@gmail.com>
2024-08-18 20:11:19 +0300

    адаптирован catalog.html

--- src/js/my-mixitup.js
+++ src/js/my-mixitup.js
@@ -13,18 +13,9 @@ if (document.querySelector(".catalog__list")) {
   });
 }
 if (document.querySelector(".contacts__content")) {
-  // if ($(window).width() > 768.99) {
-  //   var mixer = mixitup(".contacts__box", {
-  //     load: {
-  //       filter: ".all",
-  //     },
-  //   });
-  // }
-  // if ($(window).width() < 768) {
   var mixer = mixitup(".contacts__box", {
     load: {
       filter: ".map",
     },
   });
 }
-// }
