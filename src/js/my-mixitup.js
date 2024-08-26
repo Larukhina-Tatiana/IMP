7814b7385f82eccb8c72e9f3c876c0e4a0925819
Татьяна <ltasya72@gmail.com>
2024-08-26 09:50:06 +0300

    работа над receiving

--- src/js/my-mixitup.js
+++ src/js/my-mixitup.js
@@ -1,19 +1,19 @@
-if (document.querySelector(".tabs-content")) {
-  var mixer = mixitup(".tabs-content", {
+if (document.querySelector(".receiving-tabs")) {
+  var mixer1 = mixitup(".receiving-subject", {
     load: {
       filter: ".delivery",
     },
   });
 }
 if (document.querySelector(".catalog__list")) {
-  var mixer = mixitup(".catalog__list", {
+  var mixer2 = mixitup2(".catalog__list", {
     load: {
       filter: ".all",
     },
   });
 }
 if (document.querySelector(".personal-account__tabs-list")) {
-  var mixer = mixitup(".personal-account__box", {
+  var mixer3 = mixitup(".personal-account__box", {
     load: {
       filter: ".order-history",
     },
