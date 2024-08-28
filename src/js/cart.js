4a800f9bd7b7d7716cfc9f5687b26e330caccbb4
Татьяна <ltasya72@gmail.com>
2024-08-28 11:56:35 +0300

    modified:   src/js/cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,3 +5,19 @@ const cartQuantity = cart.querySelector(".header-cart__quantity");
 const fullPrice = document.querySelector(".fullprice");
 const fullQuantity = document.querySelector(".fullquantity");
 let price = 0;
+
+// Вспомогательные функции
+const randomId = () => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+
+const priceWithoutSpaces = (str) => {
+  return str.replace(/\s/g, "");
+};
+
+const normalPrice = (str) => {
+  return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
+};
