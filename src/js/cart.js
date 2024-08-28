894d0229d7a77700d3a57362e6bd8e46716e3fbd
Татьяна <ltasya72@gmail.com>
2024-08-28 11:50:49 +0300

    new file:   src/js/cart.js

---
+++ src/js/cart.js
@@ -0,0 +1,7 @@
+const btnCart = document.querySelectorAll(".btn-cart");
+const cartList = document.querySelector(".cart__list");
+const cart = document.querySelector(".header-cart__link");
+const cartQuantity = cart.querySelector(".header-cart__quantity");
+const fullPrice = document.querySelector(".fullprice");
+const fullQuantity = document.querySelector(".fullquantity");
+let price = 0;
